61788162f201a38916c7111b2b1ed47d720efb74
sbsonusunil <sbsonusunil@gmail.com>
2026-04-09 14:53:52 +0530

    project structure ready,venv setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sunil Bhatt",
+    author_email="sbsonusunil@gmail.com",
+    packages=find_packages()
+)
